ef7538b47d8979ab3212d99a6dfbce3f8f089f1f
KernelPlusAcademy <KernelPlusAcademy@gmail.com>
2025-05-30 20:08:35 -0400

    firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA4uVJOZ1ZWls6hK1b94Bm-waDgyJfYehM",
+  authDomain: "shellcoach-eacb2.firebaseapp.com",
+  projectId: "shellcoach-eacb2",
+  storageBucket: "shellcoach-eacb2.firebasestorage.app",
+  messagingSenderId: "97519723209",
+  appId: "1:97519723209:web:0ec3a0260bd3174b486318",
+  measurementId: "G-GFFE53HN8F"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
